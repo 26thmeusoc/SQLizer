fc42966471bce7f9d49cafcea81e7365cc61d64e
26thmeusoc <development@26thmeusoc.com>
2013-07-06 14:28:00 +0200

    Added License Informations

--- SQLizer/MEUMainController.h
+++ SQLizer/MEUMainController.h
@@ -1,10 +1,22 @@
-//
-//  MEUMainController.h
-//  SQLizer
-//
-//  Created by Dirk on 05.07.13.
-//  Copyright (c) 2013 Dirk "26thmeusoc" Braun. All rights reserved.
-//
+/*
+ MEUMainController.h
+ Copyright (C) 2013  Dirk "26thmeusoc" Braun
+ 
+ This file is part of SQLizer.
+ 
+ SQLizer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ SQLizer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with SQLizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <Foundation/Foundation.h>
 
